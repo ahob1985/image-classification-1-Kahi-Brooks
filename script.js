af2018c73413c1329c8c36c479e54e6167a1435f
KahiBrooks <67168011+Kahi-Brooks@users.noreply.github.com>
2021-07-30 05:45:23 +0000

    Created the demo

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 // Author:
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP;
 
 // Global ML Variables
-//let mobilenet;
+let mobilenet;
+let img; 
 
 function setup() {
-
+canvasDiv = createDiv();
+canvas = createCanvas(640, 480);
+canvas.parent(canvasDiv);
+textDiv = createDiv();
+textP = createP("Model loading, please wait...");
+textP.parent(textDiv);
+img = loadImage("images/guinea-pig.jpg");
 }
 
 function draw() {
@@ -19,9 +29,24 @@ function imageLoaded() {
 }
 
 function modelReady() {
-
+mobilenet.classify(canvas, gotResults);
 }
 
 function gotResults(error, results) {
+
+let label = results[0].label;
+let confidence = results[0].confidence;
+
+textP.html("Label: " + label + " - Confidence " + confidence);
+
+  if(error) {
+
+    console.error(error);
+
+  } else {
+
+    
+
+  }
 
 }
